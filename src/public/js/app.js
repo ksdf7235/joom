a67aa1bc609c2cba32fb29f32a933ee13de986a5
ksdf7235 <85724877+ksdf7235@users.noreply.github.com>
2021-09-02 18:02:49 +0900

    0902

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,1 +1,50 @@
-hello
+const messageList = document.querySelector("ul");
+const nickForm = document.querySelector("#nickname");
+const messageForm = document.querySelector("#message");
+const socket = new WebSocket(`ws://${
+    window.location.host}`);
+
+
+
+function makeMessage(type,payload){
+    const msg = {type,payload};
+    return JSON.stringify(msg);
+}
+
+
+
+socket.addEventListener("open", ()=> {
+    console.log("Connected to Server ✅");
+})
+
+socket.addEventListener("message",(message) => {
+    const li = document.createElement("li");
+    li.innerText = message.data;
+    messageList.append(li);
+});
+
+socket.addEventListener("close",()=> {
+    console.log("Disconnected to Server X");
+});
+
+
+function hadleSubmit(event){
+    event.preventDefault();
+    const input = messageForm.querySelector("input");
+    socket.send(makeMessage("new_message",input.value));
+    const li = document.createElement("li");
+    li.innerText = `You:${input.value}`;
+    messageList.append(li);
+    input.value= "";
+}
+
+function hadleNickSubmit(event){
+    event.preventDefault();
+    const input = nickForm.querySelector("input");
+    socket.send(makeMessage("nickname",input.value));
+    input.value= "";
+}
+
+
+messageForm.addEventListener("submit",hadleSubmit );
+nickForm.addEventListener("submit",hadleNickSubmit);
--- src/server.js
+++ src/server.js
@@ -1,4 +1,7 @@
 import express from "express";
+import WebSocket from "ws";
+import http from "http";
+import { parse } from "path";
 
 const PORT = 3000;
 const app = express();
@@ -6,8 +9,38 @@ const app = express();
 app.set('view engine',"pug");
 app.set('views',__dirname + "/views");
 app.use("/public", express.static(__dirname + "/public"));
-app.get("/",(req,res) => res.render("home"));
+app.get("/",(_,res) => res.render("home"));
 
-const handleListen = () => console.log(`✅ Server listenting on port http://localhost:${PORT} 🚀`)
+const handleListen = () =>{ 
+    console.log(`✅ Server listenting on port http://localhost:${PORT} 🚀`)
+    console.log(`✅ Server listenting on port ws://localhost:${PORT} 🚀`)
+}
 
-app.listen(PORT,handleListen);
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ server });
+
+
+const sockets = [];
+
+wss.on("connection",(socket) => {
+    sockets.push(socket);
+    socket["nickname"] = "Anon";
+    console.log("Connected to Browser ✅");
+    socket.on("close",() => console.log("Disconnected to Browser X"))
+    socket.on("message",(msg) => {
+        const translatedMessageData = msg.toString('utf8');
+        const message= JSON.parse(translatedMessageData);
+        console.log(message,translatedMessageData);
+        switch(message.type){
+        case "new_message":
+          sockets.forEach((aSocket) =>aSocket.send(`${socket.nickname}: ${message.payload}`));
+          break;
+        case "nickname":
+         socket["nickname"] = message.payload
+         break;
+
+    }
+    });
+});
+
+server.listen(PORT,handleListen);
